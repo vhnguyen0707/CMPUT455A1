b8cbecae485457929901e39d3cd308198ece6d22
Ngoc Thao Nguyen Trang <ngocthao@cs.ualberta.ca>
2021-02-27 20:30:29 -0700

    update code

--- assignment2/evaluate_state.py
+++ assignment2/evaluate_state.py
@@ -3,9 +3,23 @@ from board_util import (
     WHITE
 )
 
-VALUE_SCHEME = [0, 1, 2, 4, 12, 1000000000]
+CELLS_PER_LINE = 5
+THREE_IN_FIVE = 4
+FOUR_IN_FIVE = 12
+FIVE_IN_FIVE = 1000000000
 
-def get_line_point(state, line):
+def count_to_value(count):
+    if count == 3:
+        return THREE_IN_FIVE
+    elif count == 4:
+        return FOUR_IN_FIVE
+    elif count == 5:
+        return FIVE_IN_FIVE
+    else:
+        return count
+
+
+def get_5cell_line_value(state, line):
     count_black = 0
     count_white = 0
 
@@ -17,20 +31,20 @@ def get_line_point(state, line):
         elif cell_color == WHITE:
             count_white += 1
 
-    if count_black >= 1 and count_white >= 1:
+    if count_black > 0 and count_white > 0:
         return 0
 
     if state.current_player == BLACK:
-        return VALUE_SCHEME[count_black] - VALUE_SCHEME[count_white]
+        return count_to_value(count_black) - count_to_value(count_white)
     else:
-        return VALUE_SCHEME[count_white] - VALUE_SCHEME[count_black]
+        return count_to_value(count_white) - count_to_value(count_black)
 
 def evaluate_state_forToPlay(state):
     value = 0
     lines = state.rows + state.cols + state.diags
 
     for line in lines:
-        for i in range(len(line) - 5 + 1):
-            value += get_line_point(state, line[i:i+5])
+        for i in range(len(line) - CELLS_PER_LINE + 1):
+            value += get_5cell_line_value(state, line[i:i+CELLS_PER_LINE])
     
     return value
