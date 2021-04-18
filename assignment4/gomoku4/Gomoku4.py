4193f48a681bbff649a314fb65d757213b0f5247
“Kii-26” <55163247+Kii-26@users.noreply.github.com>
2021-04-18 21:26:03 +0900

    change moves

--- assignment4/gomoku4/Gomoku4.py
+++ assignment4/gomoku4/Gomoku4.py
@@ -9,119 +9,90 @@ from simple_board import SimpleGoBoard
 import random
 import numpy as np
 
-def undo(board,move):
-    board.board[move]=EMPTY
-    board.current_player=GoBoardUtil.opponent(board.current_player)
-
-def play_move(board, move, color):
-    board.play_move_gomoku(move, color)
-
-def game_result(board):
-    game_end, winner = board.check_game_end_gomoku()
-    moves = board.get_empty_points()
-    board_full = (len(moves) == 0)
-    if game_end:
-        #return 1 if winner == board.current_player else -1
-        return winner
-    if board_full:
-        return 'draw'
-    return None
+def count_at_depth(node, depth, nodesAtDepth):
+    if not node._expanded:
+        return
+    nodesAtDepth[depth] += 1
+    for _, child in node._children.items():
+        count_at_depth(child, depth + 1, nodesAtDepth)
 
 class GomokuSimulationPlayer(object):
-    """
-    For each move do `n_simualtions_per_move` playouts,
-    then select the one with best win-rate.
-    playout could be either random or rule_based (i.e., uses pre-defined patterns) 
-    """
-    def __init__(self, n_simualtions_per_move=10, playout_policy='random', board_size=7):
-        assert(playout_policy in ['random', 'rule_based'])
-        self.n_simualtions_per_move=n_simualtions_per_move
-        self.board_size=board_size
-        self.playout_policy=playout_policy
-
-        #NOTE: pattern has preference, later pattern is ignored if an earlier pattern is found
-        self.pattern_list=['Win', 'BlockWin', 'OpenFour', 'BlockOpenFour', 'Random']
-
-        self.name="Gomoku3"
-        self.version = 3.0
-        self.best_move=None
-    
-    def set_playout_policy(self, playout_policy='random'):
-        assert(playout_policy in ['random', 'rule_based'])
-        self.playout_policy=playout_policy
-
-    def _random_moves(self, board, color_to_play):
-        return GoBoardUtil.generate_legal_moves_gomoku(board)
-    
-    def policy_moves(self, board, color_to_play):
-        if(self.playout_policy=='random'):
-            return "Random", self._random_moves(board, color_to_play)
-        else:
-            assert(self.playout_policy=='rule_based')
-            assert(isinstance(board, SimpleGoBoard))
-            ret=board.get_pattern_moves()
-            if ret is None:
-                return "Random", self._random_moves(board, color_to_play)
-            movetype_id, moves=ret
-            return self.pattern_list[movetype_id], moves
-    
-    def _do_playout(self, board, color_to_play):
-        res=game_result(board)
-        simulation_moves=[]
-        while(res is None):
-            _ , candidate_moves = self.policy_moves(board, board.current_player)
-            playout_move=random.choice(candidate_moves)
-            play_move(board, playout_move, board.current_player)
-            simulation_moves.append(playout_move)
-            res=game_result(board)
-        for m in simulation_moves[::-1]:
-            undo(board, m)
-        if res == color_to_play:
-            return 1.0
-        elif res == 'draw':
-            return 0.0
-        else:
-            assert(res == GoBoardUtil.opponent(color_to_play))
-            return -1.0
-
-    def get_move(self, board, color_to_play):
+    def __init__(
+        self,
+        num_sim,
+        size=7,
+        exploration=0.4,
+    ):
         """
-        The genmove function called by gtp_connection
+        Player that selects a move based on MCTS from the set of legal moves
         """
-        moves=GoBoardUtil.generate_legal_moves_gomoku(board)
-        toplay=board.current_player
-        best_result, best_move=-1.1, None
-        best_move=moves[0]
-        wins = np.zeros(len(moves))
-        visits = np.zeros(len(moves))
-        while True:
-            for i, move in enumerate(moves):
-                play_move(board, move, toplay)
-                res=game_result(board)
-                if res == toplay:
-                    undo(board, move)
-                    #This move is a immediate win
-                    self.best_move=move
-                    return move
-                ret=self._do_playout(board, toplay)
-                wins[i] += ret
-                visits[i] += 1
-                win_rate = wins[i] / visits[i]
-                if win_rate > best_result:
-                    best_result=win_rate
-                    best_move=move
-                    self.best_move=best_move
-                undo(board, move)
-        assert(best_move is not None)
-        return best_move
+        self.name = "Go5"
+        self.version = "1.0"
+        self.MCTS = MCTS()
+        self.num_simulation = num_sim
+        self.exploration = exploration
+        self.simulation_policy = sim_rule
+        self.parent = None
 
-def run():
+    def reset(self):
+        self.MCTS = MCTS()
+
+    def update(self, move):
+        self.parent = self.MCTS._root
+        self.MCTS.update_with_move(move)
+
+    def get_move(self, board, toplay):
+        move = self.MCTS.get_move(
+            board,
+            toplay,
+            num_simulation=self.num_simulation,
+            exploration=self.exploration,
+        )
+        self.update(move)
+        return move
+
+    def get_node_depth(self, root):
+        MAX_DEPTH = 100
+        nodesAtDepth = [0] * MAX_DEPTH
+        count_at_depth(root, 0, nodesAtDepth)
+        prev_nodes = 1
+        return nodesAtDepth
+
+    def get_properties(self):
+        return dict(version=self.version, name=self.__class__.__name__,)
+
+
+def run(sim, sim_rule, move_filter, in_tree_knowledge):
     """
-    start the gtp connection and wait for commands.
+    Start the gtp connection and wait for commands.
     """
-    board = SimpleGoBoard(7)
-    con = GtpConnection(GomokuSimulationPlayer(), board)
+    board = GoBoard(7)
+    con = GtpConnectionGo3(
+        Go5(num_sim, sim_rule, move_filter, in_tree_knowledge), board
+    )
     con.start_connection()
 
-if __name__=='__main__':
-    run()
+
+def parse_args():
+    """
+    Parse the arguments of the program.
+    """
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--sim",
+        type=int,
+        default=300,
+        help="number of simulations per move, so total playouts=sim*legal_moves",
+    )
+    args = parser.parse_args()
+
+    num_sim = args.sim
+
+    return num_sim
+
+
+if __name__ == "__main__":
+    num_sim = parse_args()
+    run(num_sim)
